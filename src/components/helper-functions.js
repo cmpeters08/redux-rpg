84ad6630153e2eea64808ab62d7d1bc70b999eb7
TCXP1 <carolyn_peters@unigroup.com>
2018-03-07 17:04:00 -0600

    comitting before end of day

--- src/components/helper-functions.js
+++ src/components/helper-functions.js
@@ -5,26 +5,62 @@ import React, { Component } from 'react';
 export const handleBattles = (creature, player) => {
 	console.log(creature);
 	console.log(player);
+	console.log(player.health);
 
 		if(creature.defense <= player.attack){
 			const hit = player.attack - creature.defense;
 			creature.health -= hit;
-			console.log(creature.health)
-			console.log(hit);
-		}
-		if(creature.health <= 0){
-			creature.defeated = true;
-			//show win screen
+			if(creature.health <= 0){
+				creature.defeated = true;
+				console.log("Player wins");
+			}
 		}
 		if(player.defense <= creature.attack){
 			const hit = creature.attack - player.defense;
-			this.player.health -= hit;
+			player.health -= hit;
+			if(player.health <= 0){
+				player.defeated = true;
+				console.log("Player Losses");
+			}
 		}
-		if(player.health <= 0){
-			player.defeated = true;
-			//show lose screen
+		if (player.defense === creature.attack){
+			const hit = player.defense / 2;
+			player.health -= hit;
+			creature.health -= hit;
+			if(player.health <= 0 || creature.health <= 0){
+				player.defeated = true;
+				console.log("You both lose");
+			}
+			if(player.health <= 0){
+				player.defeated = true;
+				console.log("Player Losses");
+			}
+			if(creature.health <= 0){
+				creature.defeated = true;
+				console.log("Player Wins");
+			}
+		if(player.attack === creature.defense){
+			const hit = player.attack / 2;
+			player.health -= hit;
+			creature.health -= hit;
+			if(player.health <= 0 || creature.health <= 0){
+				player.defeated = true;
+				console.log("You both lose");
+			}
+			if(player.health <= 0){
+				player.defeated = true;
+				console.log("Player Losses");
+			}
+			if(creature.health <= 0){
+				creature.defeated = true;
+				console.log("Player Wins");
+			}
+
+		}
 		}
 }
+
+
 
 
 export const getRandomCreature = ( creatures ) => {
